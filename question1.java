0fe440988217af87bc1b4175575e6cce85437d75
divaa-hub <divaaparmeshwar@gmail.com>
2026-03-07 01:56:42 +0530

    Add files via upload

---
+++ question1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class question1 {
+
+    public static void main(String[] args) {
+        char ch;
+        int x=97;
+        do{
+            ch=(char)x;
+            System.out.println(ch+" ");
+            if(x%10==0)
+            break;
+            ++x;}
+            while (x<=100);
+        }}
